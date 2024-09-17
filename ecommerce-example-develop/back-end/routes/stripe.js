d6cc83323219bf4bb87bf8572eb2f915c58d64f1
Luiz Eduardo da Silva <dasilvaeduardo.ti@gmail.com>
2024-09-16 21:41:03 -0300

    ajuste do login/create user

--- ecommerce-example-develop/back-end/routes/stripe.js
+++ ecommerce-example-develop/back-end/routes/stripe.js
@@ -7,70 +7,70 @@ const router = express.Router();
 
 router.post('/create-checkout-session', async (req, res) => {
   try {
+
     const line_items = req.body.cartItems.map((item) => {
       return {
         price_data: {
-          currency: 'usd',
+          currency: 'BRL',
           product_data: {
-            name: 'algo',
-            // images: [item.image],
+            name: item.name
           },
-          unit_amount:  100, // Convertendo para centavos
+          unit_amount: 100, // Convertendo para centavos
         },
         quantity: 2, // Usa a quantidade do item no carrinho, padrão para 1
       };
     });
 
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        shipping_address_collection: {
-            allowed_countries: ['BR', 'CA', 'US'],
+      payment_method_types: ['card'],
+      shipping_address_collection: {
+        allowed_countries: ['BR', 'CA', 'US'],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 0,
+              currency: 'usd',
+            },
+            display_name: 'Frete grátis',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 5,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 7,
+              },
+            },
           },
-          shipping_options: [
-            {
-              shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                  amount: 0,
-                  currency: 'usd',
-                },
-                display_name: 'Frete grátis',
-                delivery_estimate: {
-                  minimum: {
-                    unit: 'business_day',
-                    value: 5,
-                  },
-                  maximum: {
-                    unit: 'business_day',
-                    value: 7,
-                  },
-                },
+        },
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount: 1500,
+              currency: 'usd',
+            },
+            display_name: 'Next day air',
+            delivery_estimate: {
+              minimum: {
+                unit: 'business_day',
+                value: 1,
+              },
+              maximum: {
+                unit: 'business_day',
+                value: 1,
               },
             },
-            {
-              shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                  amount: 1500,
-                  currency: 'usd',
-                },
-                display_name: 'Next day air',
-                delivery_estimate: {
-                  minimum: {
-                    unit: 'business_day',
-                    value: 1,
-                  },
-                  maximum: {
-                    unit: 'business_day',
-                    value: 1,
-                  },
-                },
-              },
-            },
-          ],
-          phone_number_collection: {
-              enabled: true,
-            },
+          },
+        },
+      ],
+      phone_number_collection: {
+        enabled: true,
+      },
       line_items,
       mode: 'payment',
       success_url: `${process.env.CLIENT_URL}/checkout-success`,
@@ -78,6 +78,7 @@ router.post('/create-checkout-session', async (req, res) => {
     });
 
     res.send({ url: session.url });
+
   } catch (error) {
     console.error('Erro ao criar sessão de checkout:', error);
     res.status(500).json({ error: 'Erro ao criar sessão de checkout' });
--- ecommerce-example-develop/front-end/src/services/auth.jsx
+++ ecommerce-example-develop/front-end/src/services/auth.jsx
@@ -1,97 +1,137 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import axios from 'axios';
+import api from './api';
 
-const initialState = {
+// const initialState = {
 
-  products: [],
-  checkedBrands: [],
-  checkedCategorys: [],
+//   products: [],
+//   checkedBrands: [],
+//   checkedCategorys: [],
+// };
+
+// export const orebiSlice = createSlice({
+//   name: "orebi",
+//   initialState,
+//   reducers: {
+//     setUserInfo: (state, action) => {
+//       state.userInfo = action.payload;
+//     },
+//     addToCart: (state, action) => {
+//       const item = state.products.find(
+//         (item) => item._id === action.payload._id
+//       );
+//       if (item) {
+//         item.quantity += action.payload.quantity;
+//       } else {
+//         state.products.push(action.payload);
+//       }
+//       toast.success("Produto adicionado ao carrinho");
+//     },
+//     increaseQuantity: (state, action) => {
+//       const item = state.products.find(
+//         (item) => item._id === action.payload._id
+//       );
+//       if (item) {
+//         item.quantity++;
+//       }
+//     },
+//     decreaseQuantity: (state, action) => {
+//       const item = state.products.find(
+//         (item) => item._id === action.payload._id
+//       );
+//       if (item.quantity === 1) {
+//         item.quantity = 1;
+//       } else {
+//         item.quantity--;
+//       }
+//     },
+//     deleteItem: (state, action) => {
+//       state.products = state.products.filter(
+//         (item) => item._id !== action.payload
+//       );
+//       toast.error("Produto removido do carrinho");
+//     },
+//     resetCart: (state) => {
+//       state.products = [];
+//     },
+//     toggleBrand: (state, action) => {
+//       const brand = action.payload;
+//       const isBrandChecked = state.checkedBrands.some(
+//         (b) => b._id === brand._id
+//       );
+//       if (isBrandChecked) {
+//         state.checkedBrands = state.checkedBrands.filter(
+//           (b) => b._id !== brand._id
+//         );
+//       } else {
+//         state.checkedBrands.push(brand);
+//       }
+//     },
+//     toggleCategory: (state, action) => {
+//       const category = action.payload;
+//       const isCategoryChecked = state.checkedCategorys.some(
+//         (b) => b._id === category._id
+//       );
+//       if (isCategoryChecked) {
+//         state.checkedCategorys = state.checkedCategorys.filter(
+//           (b) => b._id !== category._id
+//         );
+//       } else {
+//         state.checkedCategorys.push(category);
+//       }
+//     },
+
+//   },
+// });
+
+const login = async (email, password) => {
+  try {
+    const response = await api.post('auth/login', { email, password });
+
+    // Captura o token do corpo da resposta e salva no localStorage
+    const { token } = response.data;
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao realizar login:', error);
+    throw error;
+  }
 };
 
-export const orebiSlice = createSlice({
-  name: "orebi",
-  initialState,
-  reducers: {
-    setUserInfo: (state, action) => {
-      state.userInfo = action.payload;
-    },
-    addToCart: (state, action) => {
-      const item = state.products.find(
-        (item) => item._id === action.payload._id
-      );
-      if (item) {
-        item.quantity += action.payload.quantity;
-      } else {
-        state.products.push(action.payload);
-      }
-      toast.success("Produto adicionado ao carrinho");
-    },
-    increaseQuantity: (state, action) => {
-      const item = state.products.find(
-        (item) => item._id === action.payload._id
-      );
-      if (item) {
-        item.quantity++;
-      }
-    },
-    decreaseQuantity: (state, action) => {
-      const item = state.products.find(
-        (item) => item._id === action.payload._id
-      );
-      if (item.quantity === 1) {
-        item.quantity = 1;
-      } else {
-        item.quantity--;
-      }
-    },
-    deleteItem: (state, action) => {
-      state.products = state.products.filter(
-        (item) => item._id !== action.payload
-      );
-      toast.error("Produto removido do carrinho");
-    },
-    resetCart: (state) => {
-      state.products = [];
-    },
-    toggleBrand: (state, action) => {
-      const brand = action.payload;
-      const isBrandChecked = state.checkedBrands.some(
-        (b) => b._id === brand._id
-      );
-      if (isBrandChecked) {
-        state.checkedBrands = state.checkedBrands.filter(
-          (b) => b._id !== brand._id
-        );
-      } else {
-        state.checkedBrands.push(brand);
-      }
-    },
-    toggleCategory: (state, action) => {
-      const category = action.payload;
-      const isCategoryChecked = state.checkedCategorys.some(
-        (b) => b._id === category._id
-      );
-      if (isCategoryChecked) {
-        state.checkedCategorys = state.checkedCategorys.filter(
-          (b) => b._id !== category._id
-        );
-      } else {
-        state.checkedCategorys.push(category);
-      }
-    },
-    
-  },
-});
+const register = async (user) => {
+  try {
+    const response = await api.post('auth/register', user);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao realizar registro:', error); // Corrigido para registro
+    throw error;
+  }
+};
 
-export const {
-  setUserInfo,
-  addToCart,
-  increaseQuantity,
-  decreaseQuantity,
-  deleteItem,
-  resetCart,
-  toggleBrand,
-  toggleCategory,
-} = orebiSlice.actions;
-export default orebiSlice.reducer;
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
+const getCurrentUser = () => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+// export const {
+//   setUserInfo,
+//   addToCart,
+//   increaseQuantity,
+//   decreaseQuantity,
+//   deleteItem,
+//   resetCart,
+//   toggleBrand,
+//   toggleCategory,
+// } = orebiSlice.actions;
+
+// export default orebiSlice.reducer;
+
+
+export default { login, logout, register, getCurrentUser };
